03fb6a162198e1b43c5d4c9c87565a54196c7ec4
Jéssica Silva Soares <jessicassoarez@gmail.com>
2024-03-02 16:18:02 -0300

    Delete react-marketplace-main directory

--- react-marketplace-main/src/utils/loadProduct.jsx
+++
@@ -1,6 +0,0 @@
-import { productService } from "../services/productService";
-
-export async function loadProduct({ params }) {
-    const product = await productService.getProduct(params.id);
-    return { product };
-}
